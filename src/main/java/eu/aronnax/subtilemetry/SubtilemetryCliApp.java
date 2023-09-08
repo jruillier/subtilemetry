61d57f4652c3f521daf17cafc38eedd1d3c80872
Jérôme Ruillier <jerome.ruillier@aronnax.eu>
2023-09-08 09:25:43 -0400

    Adds working support of --source CLI option.

--- src/main/java/eu/aronnax/subtilemetry/SubtilemetryCliApp.java
+++ src/main/java/eu/aronnax/subtilemetry/SubtilemetryCliApp.java
@@ -1,13 +1,22 @@
 package eu.aronnax.subtilemetry;
 
+import eu.aronnax.subtilemetry.application.GenerateSrtSubtitleFromGoproMovieSourceUC;
 import picocli.CommandLine;
+
+import java.nio.file.Path;
+
+import static eu.aronnax.subtilemetry.application.GenerateSrtSubtitleFromGoproMovieSourceUC.*;
 
 @CommandLine.Command(name = "SubTelemetry", mixinStandardHelpOptions = true)
 public class SubtilemetryCliApp implements Runnable {
 
+    @CommandLine.Option(names = {"--source"}, required = true)
+    private String sourcePath;
+
     @Override
     public void run() {
-
+        GenerateSrtSubtitleFromGoproMovieSourceUC uc = new GenerateSrtSubtitleFromGoproMovieSourceUC();
+        uc.apply(new Params(Path.of(sourcePath)));
     }
 
     public static void main(String[] args) {
